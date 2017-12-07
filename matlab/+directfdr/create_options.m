4e79c460907a425b41c72ef38d014f2acdc7c8af
mnarayan <mnarayan@users.noreply.github.com>
2017-12-07 09:36:01 -0800

    Added new options for optimizing tuning parameters

--- matlab/+directfdr/create_options.m
+++ matlab/+directfdr/create_options.m
@@ -4,10 +4,17 @@ function options = create_options(varargin)
     % options.pi_0
     % options.tau_0
     % options.est_pi0
+    % options.nBoot
     
-    options.pi_0 = 0;  % Assume half of all findings are null by default.
-    options.tau_0 = .2; % T statistic threshold for null hypothesis
+    options.pi_0 = 0;  % Rejection region for p-values
+    options.tau_0 = .2; % Conservative non-rejection region for test statistics
     options.est_pi0 = [];
     
+    % Bootstrap options for parameter tuning
+    options.nBoot = 100;
+    options.Tau = linspace(.05,.5,4);
+    
+    % Speed up. 
+    options.topk = 0; % Set to 5 or 10 to speed up bootstrap iterations;
     
 end
